fda87e63e9d1ee540fbcb776a056a49f3961c99b
rankiliu <rankiliu@tencent.com>
2020-05-06 16:39:37 +0800

    Initial Commit

---
+++ xcodeAutoPacking/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  xcodeAutoPacking
+//
+//  Created by rankiliu on 2020/5/6.
+//  Copyright © 2020 rkliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ xcodeAutoPacking/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  xcodeAutoPacking
+//
+//  Created by rankiliu on 2020/5/6.
+//  Copyright © 2020 rkliu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
